56ae5a482269aea18fd4d41761619b00dfbeed62
Jonatan Villan <jonatanadrianvillan@gmail.com>
2023-11-29 20:35:30 -0300

    preentrega 2

---
+++ ejercitacion-complemetaria/ejercicio3/main.js
@@ -0,0 +1,39 @@
+//CREAR CARNET DE CONDUCION CON DATOS DE LA PERSONA.
+
+
+
+/* let dni = "36935560";
+let nombre_conductor = "jonatan villan";
+let pais = "argentina"
+let direccion = "mendoza 2543 6a";
+let fecha_nacimiento = "28/02/1992";
+let sexo = "M";
+let edad ="31";
+let altura = "1.75m";
+let peso = "68kg";
+let color_de_ojos = "marrones";
+const codigo = "555465ajd";
+
+let carnet = `codigo ${codigo}\n nombre ${nombre_conductor}\n pais${pais}\n direccion ${direccion}\n fecha de nacimiento${fecha_nacimiento}`;
+ console.log(carnet);
+*/
+
+//OBEJTOS LITERALES
+const un_objeto = {
+    edad : `31`,
+    sexo : `m`,
+    nombre : `jonatan`,
+    apellido : `villan`
+}
+
+/* console.log(un_objeto);
+
+un_objeto.dni = `36935560`;
+
+console.log(un_objeto);
+
+un_objeto.sexo = `masculino`;
+
+console.log(un_objeto); */
+
+//LLAMANDO A LOS OBJETOS CON [];
